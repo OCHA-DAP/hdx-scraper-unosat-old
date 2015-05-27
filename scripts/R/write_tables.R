0003302c4615c88c4af7e951ed8db1df9a9285d2
Luis Capelo <luiscape@gmail.com>
2015-05-27 13:10:41 -0400

    improving comments on write_table script

--- scripts/R/write_tables.R
+++ scripts/R/write_tables.R
@@ -1,24 +1,44 @@
-## Write tables in a db. ##
+#
+# Write talbes in a SQLite database.
+# Designed to work with ScraperWiki environments.
+#
+# Author: Luis Capelo | capelo@un.org
+#
+
 library(sqldf)
 
+
+#
+# Writes a data.frame to an SQLite databse.
+# By default, it will write a scraperwiki.sqlite
+# database in the root folder where the
+# script was called.
+#
 writeTable <- function(df = NULL,
-                        table_name = NULL,
-                        db = NULL,
-                        testing = FALSE,
-                        verbose = FALSE,
-                        overwrite = FALSE) {
-  # sanity check
-  if (is.null(df) == TRUE) stop("Don't forget to provide a data.frame.")
+                       table_name = NULL,
+                       db = 'scraperwiki',
+                       testing = FALSE,
+                       verbose = FALSE,
+                       overwrite = FALSE) {
+  
+  #
+  # Sanity check.
+  #
+  if(is.null(df) == TRUE) stop("Don't forget to provide a data.frame.")
   if(is.null(table_name) == TRUE) stop("Don't forget to provide a table name.")
   if(is.null(db) == TRUE) stop("Don't forget to provide a data base name.")
 
   if (verbose) message('Storing data in a database.')
 
-  # creating db
+  #
+  # Create database and establish connection.
+  #
   db_name <- paste0(db, ".sqlite")
   db <- dbConnect(SQLite(), dbname = db_name)
 
-  # force overwrite
+  #
+  # Force overwrite.
+  #
   if (overwrite == T) {
     dbWriteTable(db,
                  table_name,
@@ -26,9 +46,11 @@ writeTable <- function(df = NULL,
                  row.names = FALSE,
                  overwrite = TRUE)
   }
-
+  
+  #
+  # Inserts new values.
+  #
   else {
-    # To insert new values
     dbWriteTable(db,
                  table_name,
                  df,
@@ -36,22 +58,32 @@ writeTable <- function(df = NULL,
                  append = TRUE)
   }
 
-  # testing mode
+  #
+  # Perform tests at the time of usage.
+  #
   if(testing == TRUE) {
-    # testing for the correct table name
+    
+    #
+    # Test if table name is same
+    # as provided.
+    #
     if (table_name %in% dbListTables(db)) {
       message(paste('The table', table_name, 'is in the db.'))
     }
 
-    # testing for the table with data
-    # and sample data
-    x <- dbReadTable(db, table_name)
-    if (is.data.frame(x) == TRUE) {
-      message(paste('There is a table with', nrow(x), 'records. The head is:'))
-      print(head(x, 5))
+    #
+    # Test if tables contain data.
+    #
+    loaded_data <- dbReadTable(db, table_name)
+    if (is.data.frame(loaded_data) == TRUE) {
+      message(paste('There is a table with', nrow(loaded_data), 'records. The head is:'))
+      print(head(loaded_data, 5))
     }
   }
-
+   
+  #
+  # Disconnect.
+  #
   dbDisconnect(db)
   if (verbose) message('Done!')
 }
