c6184cf9a3187c88d697ee15281b4be52ffae2f8
Luis Capelo <luiscape@gmail.com>
2015-08-11 14:07:24 -0400

    adding checks for existing datasets and closing #34

--- scripts/hdx_register/__main__.py
+++ scripts/hdx_register/__main__.py
@@ -4,6 +4,7 @@
 import os
 import sys
 import requests
+import scraperwiki
 import yajl as json
 import progressbar as pb
 
@@ -14,6 +15,7 @@ from hdx_register import load
 from hdx_register import config
 from hdx_register import delete
 from hdx_register import create
+from termcolor import colored as color
 from utilities.prompt_format import item as I
 
 
@@ -23,7 +25,7 @@ def Main():
   #
   # Setting up configuration: dev = development; prod = production.
   #
-  p = config.FetchConfig('prod')
+  p = config.FetchConfig('dev')
   if p is not False:
 
     print "--------------------------------------------------"
@@ -49,8 +51,13 @@ def Main():
       gallery_dict = load.LoadData(os.path.join(p['json_folder'], 'gallery.json'))
 
       # Delete resources before running:
-      if p['delete_resources'] is True:
+      if p['delete_resources']:
         delete.DeleteResources(dataset_dict=dataset_dict, hdx_site=p['hdx_site'], apikey=p['hdx_key'], verbose=p['verbose'])
+
+      if p['update_all_datasets']:
+        print '--------------------------------------------------'
+        print color(u" ATTENTION:", "blue", attrs=['bold']) + ' Updating ALL datasets.'
+        print '--------------------------------------------------'
 
       #
       # Create datasets, resources, and gallery items.
@@ -67,3 +74,16 @@ def Main():
 
 if __name__ == '__main__':
   Main()
+  print '%s UNOSAT Product scraper finished successfully.\n' % I('prompt_success')
+
+
+
+# if __name__ == '__main__':
+
+#   if Main() != False:
+#     scraperwiki.status('ok')
+
+#   else:
+#     scraperwiki.status('error', 'Failed to register resources.')
+#     os("mail -s 'UNOSAT Product scraper failed' luiscape@gmail.com")
+
